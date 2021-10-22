fde9b7ab7b32bee6a334dc25146d58977d174fc2
Jerry Cao <q1063128177@gmail.com>
2021-10-22 10:54:27 +0200

    Sending

---
+++ src/main/java/Send.java
@@ -0,0 +1,24 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+
+public class Send {
+    //set up name of queue
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel()){
+            //declare a queue
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello world";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
