73b18f7c7abe5372a89f0a9a453105081937b303
Jerry Cao <q1063128177@gmail.com>
2021-10-22 14:18:24 +0200

    part2

---
+++ WorkQueue/src/main/java/Worker.java
@@ -0,0 +1,43 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class Worker {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        //tell server to deliver the message from queue
+        //we provide a callback in the form of an object that will buffer the messages until we are ready to use them
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(" [x] Done");
+            }
+        };
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+    //fake task
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+}
